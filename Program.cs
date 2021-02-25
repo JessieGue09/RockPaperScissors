ecd456668a74f2b17697f9f0a2500f6a66c9d763
JessieGue09 <jessy.v.hdez0028@gmail.com>
2021-02-25 11:06:07 -0700

    Creación de condicionales para determinar el ganador o si hay un empate. Creación de condicionales con switch case para mostrar la opción seleccionada por el jugador y por  el AI.

--- Program.cs
+++ Program.cs
@@ -35,6 +35,101 @@ namespace RockPaperScissors
             return option;
         }
 
+
+        /// <summary>
+        /// Creación de condicionales para indicar que opcion seleccionaron el jugador y el AI.
+        /// Creación de condicionales para indicar si hay un ganador entre el jugador y el AI. Si los dos quedan empate, muestra con un comentario que empataron.
+        /// </summary>
+        /// <param name="option">Opcion seleccionada por el jugador</param>
+        /// <param name="aiOption">Opcion seleccionada por la computadora</param>
+        static void CheckWinner(int option, int aiOption) 
+        {
+            // 1: piedra
+            // 2: papel
+            // 3: tijera
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Jugador: Piedra");
+                    break;
+                case 2:
+                    Console.WriteLine("Jugador: Papel");
+                    break;
+                case 3: 
+                    Console.WriteLine("Jugador: Tijera");
+                    break;
+                
+            }
+
+            switch (aiOption)
+            {
+                case 1:
+                    Console.WriteLine("AI: Piedra");
+                    break;
+                case 2:
+                    Console.WriteLine("AI: Papel");
+                    break;
+                case 3: 
+                    Console.WriteLine("AI: Tijera");
+                    break;
+                
+            }
+
+            //piedra < tijera < papel < piedra
+            // 1 < 2 < 3 < 1
+
+            if (option == 1) // Jugador: Piedra
+            {
+                switch (aiOption)
+                {
+                    case 1: // AI: Piedra
+                        Console.WriteLine("Eempate");
+                        break;
+                    case 2: // AI: Papel
+                        Console.WriteLine("Gana AI");
+                        break;
+                    case 3: // AI: Tijera
+                        Console.WriteLine("Gana Jugador");
+                        break;
+                    
+                }
+            }
+            else if (option == 2) // Jugador: Papel
+            {
+                switch (aiOption)
+                {
+                    case 1: // AI: Piedra
+                        Console.WriteLine("Gana Jugador");
+                        break;
+                    case 2: // AI: Papel
+                        Console.WriteLine("Empate");
+                        break;
+                    case 3: // AI: Tijera
+                        Console.WriteLine("Gana AI");
+                        break;
+                    
+                }
+            }
+            else if (option == 3) // Jugador: Tijera
+            {
+                switch (aiOption)
+                {
+                    case 1: // AI: Piedra
+                        Console.WriteLine("Gana AI");
+                        break;
+                    case 2: // AI: Papel
+                        Console.WriteLine("Gana Jugador");
+                        break;
+                    case 3: // AI: Tijera
+                        Console.WriteLine("Empate");
+                        break;
+                    
+                }
+            }
+
+            
+        }
+
         static void Main(string[] args)
         {
             int option = SelectOption();
@@ -43,8 +138,10 @@ namespace RockPaperScissors
             //TODO: La computadora genera su opción a jugar de manera aleatoria (piedra, papel o tijera)
             int aiOption = MostrarNumeroRandom();
             Console.WriteLine("Opcion seleccionada aleatoriamente: " + aiOption);
-            //TODO: Determinar quién ganó de acuerdo a las opciones seleccionadas, y mostrar al ganador
 
+            //TODO: Determinar quién ganó de acuerdo a las opciones seleccionadas, y mostrar al ganador
+            CheckWinner(option, aiOption);
+            // Pidra > tijera > papel
             //TODO: Jugar 3 partidas, y mencionar quién ganó al final (jugador o computadora)
 
         }
